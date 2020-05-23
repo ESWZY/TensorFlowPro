25e76f93dffffa52e7613cb8500835f346a22469
ESWZY <1006460505@qq.com>
2020-05-23 18:18:09 +0800

    Add logistic regression

--- 无监督学习的网络用户行为分析/ml_algorithms/__init__.py
+++ 无监督学习的网络用户行为分析/ml_algorithms/__init__.py
@@ -1,3 +1,4 @@
 from .k_means import KMeansAlgorithm
 from .random_forest import RandomForestAlgorithm
 from .svm import SVMAlgorithm
+from .logistic_regression import LogisticRegressionAlgorithm
---
+++ 无监督学习的网络用户行为分析/ml_algorithms/logistic_regression.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+import matplotlib
+import matplotlib.pyplot as plt
+from ml_algorithms.ml_algorithm_interface import AlgorithmInterface
+from sklearn import metrics
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, precision_score, recall_score, auc
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
+matplotlib.use('TkAgg')
+
+class LogisticRegressionAlgorithm(AlgorithmInterface):
+    def __init__(self):
+        super(LogisticRegressionAlgorithm, self).__init__()
+
+    def train_phase(self):
+        pipe_logistic_regression = Pipeline([('sc', StandardScaler()),
+                                            ('pca', PCA(n_components=2)),
+                                            ('clf', LogisticRegression(random_state=1))
+                                             ])
+
+        # param_range = [10 ** c for c in range(-4, 4)]
+        param_range = [0.0001, 0.001]
+        hyper_parameter_grid = {
+            'clf__C': param_range,
+            'clf__gamma': param_range,
+            'clf__kernel': ['linear', 'rbf']
+        }
+
+        # 设置4折交叉验证随机搜索
+        self.classifier = RandomizedSearchCV(estimator=pipe_logistic_regression,
+                                             param_distributions=hyper_parameter_grid,
+                                             cv=4, n_iter=5,
+                                             scoring='roc_auc',
+                                             n_jobs=-1, verbose=2,
+                                             return_train_score=True,
+                                             random_state=42)
+
+        # 开始训练
+        self.classifier.fit(self.train_data, self.train_label)
+        print("训练结束")
+
+    def test_phase(self):
+        y_predict = self.classifier.predict(self.test_data)
+        print("准确度: %f" % accuracy_score(self.test_label, y_predict))
+        print("精确度: %f" % precision_score(self.test_label, y_predict, average="macro"))
+        print("召回率: %f" % recall_score(self.test_label, y_predict, average="macro"))
+
+        fpr, tpr, thresholds = metrics.roc_curve(y_predict, self.test_label)
+        plt.plot(fpr, tpr, marker='o')
+        plt.show()
+        auc_score = auc(fpr, tpr)
+        print("AUC: %f" % auc_score)
