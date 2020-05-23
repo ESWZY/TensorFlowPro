79e4b42a330196096c01fb95c98f4398449bc4f7
ESWZY <1006460505@qq.com>
2020-05-23 18:04:58 +0800

    K-Means reformat code

---
+++ 无监督学习的网络用户行为分析/main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from ml_algorithms import KMeansAlgorithm
+
+if __name__ == '__main__':
+    km = KMeansAlgorithm()
+    km.run()
--- 无监督学习的网络用户行为分析/ml_algorithms/k_means.py
+++ 无监督学习的网络用户行为分析/ml_algorithms/k_means.py
@@ -1,8 +1,10 @@
-from .ml_algorithm_interface import AlgorithmBaseInterface
-import tensorflow as tf
-import numpy
 from random import shuffle
+
+import numpy
+import tensorflow as tf
 from numpy import array
+
+from .ml_algorithm_interface import AlgorithmBaseInterface
 
 
 class KMeansAlgorithm(AlgorithmBaseInterface):
@@ -12,6 +14,28 @@ class KMeansAlgorithm(AlgorithmBaseInterface):
 
     def __init__(self):
         self.DATA_PATH = "data_10000.csv"  # 未添加label的数据集
+
+        # tcp -> 1
+        # icmp - > 2
+        # udp -> 3
+
+        # http -> 1
+        # whois -> 2
+        # vmnet -> 3
+        # 其他的按照字母表顺序从4排到59
+
+        # SF -> 1
+        # S0 -> 2
+        # S1 -> 3
+        # S2 -> 4
+        # REJ -> 5
+        # RSTR -> 6
+
+        # normal. -> 1
+        # back. -> 2
+        # neptune. -> 3
+        # smurf. -> 4
+
         super(KMeansAlgorithm, self).__init__()
 
     def handle(self, data_matrix):
