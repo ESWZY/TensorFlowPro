b879c18885497e18f2b3d57554311f065a63273e
ESWZY <1006460505@qq.com>
2020-05-29 17:06:59 +0800

    Add voting classifier

--- 无监督学习的网络用户行为分析/main.py
+++ 无监督学习的网络用户行为分析/main.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from sklearn.ensemble import VotingClassifier
 
 from ml_algorithms import (
     KMeansAlgorithm,
@@ -6,6 +7,18 @@ from ml_algorithms import (
     SVMAlgorithm,
     LogisticRegressionAlgorithm
 )
+
+
+def real_predict(test_data, rfa, svma, lra, vclf):
+    rfa_predeit = rfa.classifier.predict(test_data[0:1])[0]
+    svma_predeit = svma.classifier.predict(test_data[0:1])[0]
+    lra_predeit = lra.classifier.predict(test_data[0:1])[0]
+    vclf_predict = vclf.predict(test_data[0:1])[0]
+    print('RF预测结果：' + str(rfa_predeit))
+    print('SVM预测结果：' + str(svma_predeit))
+    print('LR预测结果：' + str(lra_predeit))
+    print('Voting预测结果：' + str(vclf_predict))
+
 
 if __name__ == '__main__':
     kma = KMeansAlgorithm()
@@ -19,3 +32,15 @@ if __name__ == '__main__':
 
     lra = LogisticRegressionAlgorithm()
     lra.run()
+
+    test_data = rfa.test_data
+    test_label = rfa.test_label
+
+    vclf = VotingClassifier(estimators=[('rfa', rfa.classifier), ('svma', svma.classifier), ('lra', lra.classifier)])
+    vclf = vclf.fit(rfa.test_data, rfa.test_label)
+    accuracy_score = vclf.score(test_data, test_label)
+    print("准确度: %f" % accuracy_score)
+
+    for i in range(1, 1000):
+        print('————————————————————')
+        real_predict(test_data[i:i+1], rfa, svma, lra, vclf)
